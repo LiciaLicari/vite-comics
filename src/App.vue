4319799b0900eb448f2b961642fb74a064dbabff
“LiciaLicari” <licianbsplicari@gmail.com>
2023-09-22 17:32:21 +0200

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -19,14 +19,46 @@ export default{
 <template>
 
   <header id="app_header">
-    <div class="logo">
-      <img src="./assets/img/dc-logo.png" alt="">
-    </div>
-    <nav class="navbar navbar-expand navbar-light bg-light">
-        <div class="nav navbar-nav">
-            <a class="nav-item nav-link active" href="#" aria-current="page">Home <span class="visually-hidden">(current)</span></a>
-            <a class="nav-item nav-link" href="#">Home</a>
+    <nav class="navbar navbar-expand-lg">
+      <div class="container d-flex justify-content-between">
+        <div class="logo">
+          <img src="./assets/img/dc-logo.png" alt="">
         </div>
+        <div class="nav navbar-nav">
+          <li class="nav-item">
+            <a href="" class="nav-link active">CHARACTERS</a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">COMICS</a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">MOVIES</a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">TV</a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">GAMES</a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">COLLECTIBLES</a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">VIDEOS</a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">FANS</a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">NEWS</a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">SHOP</a>
+          </li>
+            
+        </div>
+
+      </div>
     </nav>
   </header>
   <!-- /#app_header -->
@@ -44,6 +76,6 @@ export default{
 </template>
 
 
-<style lang = "scss">
+<style lang="scss">
 @use './assets/scss/app.scss';
 </style>
