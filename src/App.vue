6ff3bf473a918fde93e2d93e32cd3c30957612ca
“LiciaLicari” <licianbsplicari@gmail.com>
2023-09-22 16:12:26 +0200

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script>
+export default{
+  name: 'App',
+  data(){
+    return {
+
+    }
+  },
+//   components: {
+//     AppHeader,
+//     AppMain,
+//     AppLink,
+//     AppFooter
+// }
+
+}
+</script>
+
+<template>
+
+  <header id="app_header"></header>
+  <!-- /#app_header -->
+  <main id="app_main">
+    <section id="hero_section"></section>
+    <section id="menu"></section>
+  </main>
+  <!-- /#app_main -->
+  <footer id="app_footer">
+    <section id="site_map"></section>
+    <section id="social"></section>
+  </footer>
+  <!-- /#app_footer -->
+ 
+</template>
+
+
+<style lang = "scss">
+@use './assets/scss/app.scss';
+</style>
