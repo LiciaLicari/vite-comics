f692411c549e7382ebfff396696227d769e6deac
“LiciaLicari” <licianbsplicari@gmail.com>
2023-09-22 16:47:47 +0200

    add logo+navbar

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,17 @@ export default{
 
 <template>
 
-  <header id="app_header"></header>
+  <header id="app_header">
+    <div class="logo">
+      <img src="./assets/img/dc-logo.png" alt="">
+    </div>
+    <nav class="navbar navbar-expand navbar-light bg-light">
+        <div class="nav navbar-nav">
+            <a class="nav-item nav-link active" href="#" aria-current="page">Home <span class="visually-hidden">(current)</span></a>
+            <a class="nav-item nav-link" href="#">Home</a>
+        </div>
+    </nav>
+  </header>
   <!-- /#app_header -->
   <main id="app_main">
     <section id="hero_section"></section>
@@ -35,5 +45,5 @@ export default{
 
 
 <style lang = "scss">
-@use '../assets/scss/app.scss';
+@use './assets/scss/app.scss';
 </style>
